5795f486f08fa3d0c6c84dc8312b1297d3ab1a17
mzouink <zouinkhi.marwan@gmail.com>
2022-09-30 13:52:05 -0400

    github reader

---
+++ src/main/java/org/janelia/saalfeldlab/n5/github/N5GithubReader.java
@@ -0,0 +1,193 @@
+/*
+ * *
+ *  * Copyright (c) 2022, Janelia
+ *  * All rights reserved.
+ *  *
+ *  * Redistribution and use in source and binary forms, with or without
+ *  * modification, are permitted provided that the following conditions are met:
+ *  *
+ *  * 1. Redistributions of source code must retain the above copyright notice,
+ *  *    this list of conditions and the following disclaimer.
+ *  * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *  *    this list of conditions and the following disclaimer in the documentation
+ *  *    and/or other materials provided with the distribution.
+ *  *
+ *  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *  * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.janelia.saalfeldlab.n5.github;
+
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import net.imglib2.cache.img.CachedCellImg;
+import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.numeric.real.FloatType;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.lib.ObjectLoader;
+import org.janelia.saalfeldlab.n5.*;
+import org.janelia.saalfeldlab.n5.github.lib.GithubRepo;
+import org.janelia.saalfeldlab.n5.imglib2.N5Utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+public class N5GithubReader extends AbstractGsonReader implements N5Reader {
+	private final GithubRepo repo;
+
+	public N5GithubReader(String url, String branch) throws IOException, GitAPIException {
+		this(new GithubRepo(url,branch), new GsonBuilder());
+	}
+
+	private N5GithubReader(GithubRepo repo,  GsonBuilder gsonBuilder) {
+		super(gsonBuilder);
+		this.repo = repo;
+	}
+
+	public boolean exists(String pathName) {
+		String fullPath = this.getFullPath(pathName);
+		if (fullPath.isEmpty())
+			return true;
+		boolean result = repo.exists(pathName);
+		System.out.println("File "+pathName+" : "+result);
+		return result;
+	}
+
+	public HashMap<String, JsonElement> getAttributes(String pathName) throws IOException {
+		String attributesKey = this.getAttributesKey(pathName);
+		if (!repo.exists(attributesKey)){
+			System.out.println("Attribute key not found "+attributesKey);
+			return new HashMap();
+		} else {
+			InputStream in = this.readGithubObject(attributesKey);
+			Throwable var4 = null;
+
+			HashMap var5;
+			try {
+				var5 = GsonAttributesParser.readAttributes(new InputStreamReader(in), this.gson);
+			} catch (Throwable var14) {
+				var4 = var14;
+				throw var14;
+			} finally {
+				if (in != null) {
+					if (var4 != null) {
+						try {
+							in.close();
+						} catch (Throwable var13) {
+							var4.addSuppressed(var13);
+						}
+					} else {
+						in.close();
+					}
+				}
+
+			}
+
+			return var5;
+		}
+	}
+
+	public DataBlock<?> readBlock(String pathName, DatasetAttributes datasetAttributes, long... gridPosition) throws IOException {
+		String dataBlockKey = this.getDataBlockKey(pathName, gridPosition);
+		if (!repo.exists(dataBlockKey)){
+			System.out.println("Attribute key not found "+dataBlockKey);
+			return null;
+		} else {
+			InputStream in = this.readGithubObject(dataBlockKey);
+			Throwable var6 = null;
+
+			DataBlock var7;
+			try {
+				var7 = DefaultBlockReader.readBlock(in, datasetAttributes, gridPosition);
+			} catch (Throwable var16) {
+				var6 = var16;
+				throw var16;
+			} finally {
+				if (in != null) {
+					if (var6 != null) {
+						try {
+							in.close();
+						} catch (Throwable var15) {
+							var6.addSuppressed(var15);
+						}
+					} else {
+						in.close();
+					}
+				}
+
+			}
+
+			return var7;
+		}
+	}
+
+	public String[] list(String pathName) throws IOException {
+		String fullPath = this.getFullPath(pathName);
+		if (fullPath.isEmpty())
+			return repo.getFiles().keySet().stream().toArray(String[]::new);
+		else
+			return repo.getFiles().keySet().stream().filter(e -> e.contains(fullPath)).toArray(String[]::new);
+	}
+
+	protected InputStream readGithubObject(String objectKey) throws IOException {
+		ObjectLoader loader = repo.readFile(objectKey);
+		return loader.openStream();
+	}
+
+	protected static String replaceBackSlashes(String pathName) {
+		return pathName.replace("\\", "/");
+	}
+
+	protected static String removeLeadingSlash(String pathName) {
+		return !pathName.startsWith("/") && !pathName.startsWith("\\") ? pathName : pathName.substring(1);
+	}
+
+	protected static String addTrailingSlash(String pathName) {
+		return !pathName.endsWith("/") && !pathName.endsWith("\\") ? pathName + "/" : pathName;
+	}
+
+	protected String getDataBlockKey(String datasetPathName, long... gridPosition) {
+		String[] pathComponents = new String[gridPosition.length];
+
+		for(int i = 0; i < pathComponents.length; ++i) {
+			pathComponents[i] = Long.toString(gridPosition[i]);
+		}
+
+		String dataBlockPathName = Paths.get(removeLeadingSlash(datasetPathName), pathComponents).toString();
+		return this.getFullPath(dataBlockPathName);
+	}
+
+	protected String getFullPath(String relativePath) {
+return removeLeadingSlash(replaceBackSlashes(relativePath));
+	}
+
+	protected String getAttributesKey(String pathName) {
+		String attributesPath = Paths.get(removeLeadingSlash(pathName), "attributes.json").toString();
+		return this.getFullPath(attributesPath);
+	}
+
+	public static void main(String[] args) throws GitAPIException, IOException {
+		String url = "https://github.com/mzouink/n5_image.git";
+		String branch = "main";
+		String dataset = "setup0/timepoint0/s0";
+
+		N5Reader reader = new N5GithubReader(url,branch);
+		System.out.println("Created");
+		CachedCellImg<FloatType, ?> img = N5Utils.open(reader, dataset);
+		ImageJFunctions.show(img);
+	}
+
+}
---
+++ src/main/java/org/janelia/saalfeldlab/n5/github/lib/GithubRepo.java
@@ -0,0 +1,130 @@
+/*
+ * *
+ *  * Copyright (c) 2022, Janelia
+ *  * All rights reserved.
+ *  *
+ *  * Redistribution and use in source and binary forms, with or without
+ *  * modification, are permitted provided that the following conditions are met:
+ *  *
+ *  * 1. Redistributions of source code must retain the above copyright notice,
+ *  *    this list of conditions and the following disclaimer.
+ *  * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *  *    this list of conditions and the following disclaimer in the documentation
+ *  *    and/or other materials provided with the distribution.
+ *  *
+ *  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ *  * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ *  * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ *  * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ *  * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ *  * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ *  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  * POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+package org.janelia.saalfeldlab.n5.github.lib;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.internal.storage.dfs.DfsRepositoryDescription;
+import org.eclipse.jgit.internal.storage.dfs.InMemoryRepository;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.transport.RefSpec;
+import org.eclipse.jgit.treewalk.TreeWalk;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class GithubRepo {
+
+	private final String url;
+	private final Map<String, ObjectId> files;
+	private final Git git;
+	private final String branch;
+
+
+	public GithubRepo(String url, String branch) throws GitAPIException, IOException {
+		this.url = url;
+		this.branch = branch;
+		this.git = createGit(url);
+		RevTree tree = createTree(git, branch);
+		this.files = mapFiles(git, tree);
+	}
+
+	@Override
+	public String toString() {
+		return "GithubRepo{" +
+				"url='" + url + '\'' +
+				", branch='" + branch + '\'' +
+				'}';
+	}
+
+	private static Map<String, ObjectId> mapFiles(Git git, RevTree tree) throws IOException {
+		Map<String, ObjectId> result = new HashMap<>();
+		TreeWalk treeWalk = new TreeWalk(git.getRepository());
+		treeWalk.addTree(tree);
+		treeWalk.setRecursive(true);
+		while (treeWalk.next()) {
+			result.put(treeWalk.getPathString(), treeWalk.getObjectId(0));
+		}
+		return result;
+	}
+
+	private static RevTree createTree(Git git, String branch) throws IOException {
+		Repository repo = git.getRepository();
+		repo.getObjectDatabase();
+		ObjectId lastCommitId = repo.resolve("refs/heads/" + branch);
+		RevWalk revWalk = new RevWalk(repo);
+		RevCommit commit = revWalk.parseCommit(lastCommitId);
+		return commit.getTree();
+	}
+
+	private static Git createGit(String url) throws GitAPIException {
+		DfsRepositoryDescription repoDesc = new DfsRepositoryDescription();
+		InMemoryRepository repo = new InMemoryRepository(repoDesc);
+		Git git = new Git(repo);
+		git.fetch()
+				.setRemote(url)
+				.setRefSpecs(new RefSpec("+refs/heads/*:refs/heads/*"))
+				.call();
+		return git;
+	}
+
+	public boolean exists(String path) {
+		return this.files.containsKey(path);
+	}
+
+	public ObjectLoader readFile(String path) throws IOException {
+		if (!exists(path))
+			throw new IOException("File doesn't exist ! :" + path);
+
+		return git.getRepository().open(files.get(path));
+	}
+
+	public Map<String, ObjectId> getFiles() {
+		return files;
+	}
+
+	public static void main(String[] args) throws GitAPIException, IOException {
+		GithubRepo githubRepo = new GithubRepo("https://github.com/github/testrepo.git", "master");
+		System.out.println("created !");
+		Map<String, ObjectId> files = githubRepo.getFiles();
+
+		for (String k :
+				files.keySet()) {
+			System.out.println(k + ":" + files.get(k).getName());
+		}
+
+		githubRepo.readFile("test/alloc.c").copyTo(System.out);
+	}
+}
